82a115476538445815f05302ec2b24eed5caec9e
Austin Jackson <au5ton@icloud.com>
2015-09-06 17:45:01 -0500

    Holy cow, lots of stuff

--- chattest/app.js
+++ chattest/app.js
@@ -1,9 +1,17 @@
+//From:
+// http://cwbuecheler.com/web/tutorials/2013/node-express-mongo/
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+// New Code
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +29,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- chattest/routes/index.js
+++ chattest/routes/index.js
@@ -1,9 +1,95 @@
 var express = require('express');
 var router = express.Router();
 
+//Sample chatHistory format
+var ch = [
+    {
+        message: "Happy new year!",
+        sentAt: "1/1/2015",
+        sentBy: "fluff"
+    },
+    {
+        message: "Christmas was awesome!",
+        sentAt: "12/25/2014",
+        sentBy: "zzz"
+    },
+    {
+        message: "What's up, man?",
+        sentAt: "12/24/2014",
+        sentBy: "skream"
+    },
+    {
+        message: "What's up, man?",
+        sentAt: "12/24/2014",
+        sentBy: "skream"
+    },
+    {
+        message: "What's up, man?",
+        sentAt: "12/24/2014",
+        sentBy: "skream"
+    },
+    {
+        message: "What's up, man?",
+        sentAt: "12/24/2014",
+        sentBy: "skream"
+    },
+    {
+        message: "What's up, man?",
+        sentAt: "12/24/2014",
+        sentBy: "skream"
+    },
+    {
+        message: "What's up, man?",
+        sentAt: "12/24/2014",
+        sentBy: "skream"
+    },
+    {
+        message: "What's up, man?",
+        sentAt: "12/24/2014",
+        sentBy: "skream"
+    },
+    {
+        message: "What's up, man?",
+        sentAt: "12/24/2014",
+        sentBy: "skream"
+    },
+    {
+        message: "What's up, man?",
+        sentAt: "12/24/2014",
+        sentBy: "skream"
+    },
+    {
+        message: "Here are my dank memes: imgur.com",
+        sentAt: "12/23/2014",
+        sentBy: "soot"
+    }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    // var db = req.db;
+    // var collection = db.get('usercollection');
+    // collection.find({},{},function(e,docs){
+    //     console.log(JSON.stringify(docs));
+    //     // res.render('userlist', {
+    //     //     "userlist" : docs.
+    //     // });
+    // });
+
+    //Retrieve chat history from database
+    //Save last 50 messages
+    //Reverse array order, put the newest messages at the top
+
+    res.render('index', { title: 'Express', chatHistory: ch });
+});
+
+
+
+router.post('/', function(req, res, next) {
+    console.log(req.headers["user-agent"]);
+    console.log(req.body);
+    res.render('index', { title: 'Express', chatHistory: ch });
 });
 
 module.exports = router;
