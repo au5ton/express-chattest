f693bab961b3584eae7c6ffddd182189c3db3e58
Austin Jackson <au5ton@icloud.com>
2015-09-07 21:06:00 -0500

    Further work to a RESTful API

--- chattest/routes/api/v1.js
+++ chattest/routes/api/v1.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+//var User = require('../../models/user');
+var ChatMessage = require('../../models/chatmessage');
+
 //Generate JSON response
 function jsonr(head_message,body_message,code_type) {
     return JSON.stringify({
@@ -12,7 +15,7 @@ function jsonr(head_message,body_message,code_type) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send(jsonr("Successfully connected to API v1",null,"nonerror"));
+    res.send(jsonr("Successfully connected to API v1",null,"success"));
 });
 
 router.get('/messages', function(req, res, next) {
@@ -21,10 +24,27 @@ router.get('/messages', function(req, res, next) {
     //truncate last 50 messages
     //Reverse array order, put the newest messages at the top
 
+    var history = [];
+
+    ChatMessage.find({}, function(err, messages) {
+        if (err) throw err;
+        history = messages;
+    });
+
+    if(!history.length) {
+        res.send(jsonr("Couldn\'t retrieve any ChatMessages", null,"error"));
+    }
+
+    res.send(history);
+
 });
 
 router.post('/messages', function(req, res, next) {
-    //
+    var newMessage;
+    var request = req.body;
+    console
+    if(req.body.message.)
+    res.send();
 });
 
 module.exports = router;
--- chattest/routes/index.js
+++ chattest/routes/index.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-//Sample chatHistory format
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
-});
-
-
-
-router.post('/', function(req, res, next) {
-    console.log(req.headers["user-agent"]);
-    console.log(req.body);
-    res.render('index', { title: 'Express', chatHistory: ch });
 });
 
 module.exports = router;
